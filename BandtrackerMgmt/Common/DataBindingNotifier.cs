f5d3077308c79a1eeb820b453c34e35cc1c11603
Johan Smet <johan.smet@justcode.be>
2015-12-08 22:22:46 +0100

    Create ViewModelMainWindow

---
+++ BandtrackerMgmt/Common/DataBindingNotifier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace BandtrackerMgmt
+{
+    class DataBindingNotifier : INotifyPropertyChanged
+    {
+        internal void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+        protected bool SetField<T>(ref T p_field, T p_value, [CallerMemberName] string p_property = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(p_field, p_value))
+                return false;
+
+            p_field = p_value;
+            RaisePropertyChanged(p_property);
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ BandtrackerMgmt/ViewModel/ViewModelMainWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandtrackerMgmt
+{
+    class ViewModelMainWindow : ViewModelBase
+    {
+
+        // properties
+        public string WindowTitle { get { return m_window_title; } set { SetField(ref m_window_title, value); } }
+
+        // variables.ViewModel
+        private string m_window_title = "BandTracker Management";
+    }
+}
